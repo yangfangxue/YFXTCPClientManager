8824d5fd8c9e4c7432130b2c45f6a0a4a75c474b
xue-ios <122092669@qq.com>
2017-04-18 13:37:51 +0800

    Initial Commit

---
+++ YFX_TCPClientManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFX_TCPClientManager
+//
+//  Created by fangxue on 2017/4/18.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YFX_TCPClientManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YFX_TCPClientManager
+//
+//  Created by fangxue on 2017/4/18.
+//  Copyright © 2017年 fangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
